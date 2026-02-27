d9df842f62422723ea20e5cbb0662a79c1c1d222
H0vak <novak.anton2k8@gmail.com>
2026-02-27 19:29:19 +0200

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b, c;
+    cout << "Введіть a b c (ціна, гроші, кількість пляшок в автоматі): ";
+    if (!(cin >> a >> b >> c)) return 0;
+
+    int canBuy = b / a;
+    int answer = min(canBuy, c);
+
+    cout << "Максимальна кількість пляшок: " << answer << endl;
+
+    return 0;
+}
